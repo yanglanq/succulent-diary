1610b54e949cf12a1ef6774f66d0590eb220cb9e
yanglanq <2873435684@qq.com>
2020-05-02 20:08:29 +0800

    引入font

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,12 +1,11 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+let {duorou1,duorou2} = require("../../utils/iconfont")
 Page({
   data: {
-   
+   duorou1:duorou2
   },
   onLoad: function () {
-   
   }
 })
