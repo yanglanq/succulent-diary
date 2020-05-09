b3ea7c23f739320321d7afd1ffe9b8c6477467f4
yanglanq <2873435684@qq.com>
2020-05-09 20:28:40 +0800

    首页以及搜索部分完成

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    hotKeyList:["多rt肉","多肉","多rth肉","多zdg肉","多肉","多srtn肉","多肉","多肉","多肉","多肉","多肉","多肉"],
+    // searchList:[{
+    //   imgUrl:"https://yanglq.xyz/images/ziyuan1.png",
+    //   name:"清盛锦",
+    //   alias:"艳日晖  灿烂  艳日辉",
+    //   intro:"清盛锦又称“灿烂”，为景天科、莲花掌属多年生肉质草本植物，清盛锦株形秀丽美观，叶片色彩绚丽斑斓，常用小型工艺盆栽种，装饰窗台、几架、..."
+    // }]
+    searchList:[]
   },
 
   /**
