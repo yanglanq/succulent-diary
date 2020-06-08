a49e2738f28176e7de5285631d266fa80ea4b884
yanglanq <2873435684@qq.com>
2020-06-08 11:53:18 +0800

    和基本接口

--- miniprogram/pages/diaryList/diaryList.js
+++ miniprogram/pages/diaryList/diaryList.js
@@ -5,9 +5,10 @@ Page({
      * 页面的初始数据
      */
     data: {
-        thisDiary:{
+        diary:{
             name:"日记名称"
         },
+        imgList:[],
         diaryList:[
             {
                 
@@ -35,6 +36,52 @@ Page({
 		})
 		this.setData({
 			card: {}
+		})
+    },
+    TimeChange(e){
+        let time = "diary.time"
+        this.setData({
+            [time]:e.detail.data
+        })
+    },
+    ChooseImage() {
+		wx.chooseImage({
+			count: 1, //默认9
+			sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+			sourceType: ['album'], //从相册选择
+			success: (res) => {
+				if (this.data.imgList.length != 0) {
+					this.setData({
+						imgList: this.data.imgList.concat(res.tempFilePaths)
+					})
+				} else {
+					this.setData({
+						imgList: res.tempFilePaths
+					});
+				}
+			}
+		});
+	},
+	ViewImage(e) {
+		wx.previewImage({
+			urls: this.data.imgList,
+			current: e.currentTarget.dataset.url
+		});
+	},
+	DelImg(e) {
+		wx.showModal({
+			title: '',
+			content: '确定要删除吗？',
+			cancelText: '取消',
+			confirmText: '确定',
+			success: res => {
+				if (res.confirm) {
+					this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+					this.setData({
+						imgList: this.data.imgList
+					})
+				}
+			}
 		})
 	},
 })
