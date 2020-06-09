bba0c11bd28e1d27139a4342c295e1da0f24353a
yanglanq <2873435684@qq.com>
2020-06-09 19:25:18 +0800

    授权登录

--- miniprogram/pages/authorize/authorize.js
+++ miniprogram/pages/authorize/authorize.js
@@ -1,4 +1,5 @@
 // pages/authorize/authorize.js
+let app = getApp();
 Page({
 
   /**
@@ -11,6 +12,56 @@ Page({
   getUserInfo(e){
     console.log(e);
     if(e.detail.userInfo){
+      app.globalData.userInfo = e.detail.userInfo
+      wx.login({
+        success(res) {
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: 'https://yanglq.xyz/user/login',
+              data: {
+                code: res.code
+              },
+              success: res => {
+                var id = res.data;
+                if (id < 0) {
+                  id = -id;
+                  wx.getUserInfo({
+                    success: res1 => {
+                      var username = res1.userInfo.nickName;
+                      var sex = res1.userInfo.gender;
+                      var headUrl = res1.userInfo.avatarUrl;
+                      that.globalData.userInfo = res1.userInfo;
+                      wx.request({
+                        url: 'https://yanglq.xyz/user/addUser',
+                        data: {
+                          'id': id,
+                          'username': username,
+                          'headUrl': headUrl,
+                          'sex': sex,
+                        },
+                        method: 'GET',
+                        success: function (res) {
+                          console.log(res);
+                        },
+                      })
+                    },
+                    fail: function () {
+                      console.log("获取失败！")
+                    },
+                    complete: function () {
+                      console.log("获取用户信息完成！")
+                    }
+                  })
+                }
+                that.globalData.userInfo.uid = id;
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
       wx.switchTab({
         url: '../index/index'
       })
@@ -20,24 +71,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.getSetting({
-      success: function(res) {
-        if(res.authSetting['scope.userInfo']){
-          wx.getUserInfo({
-            success: function(res){
-              // success
-              // console.log(res.userInfo);
-            },
-            fail: function() {
-              // fail
-            },
-            complete: function() {
-              // complete
-            }
-          })
-        }
-      }
-    });
+    // wx.getSetting({
+    //   success: function(res) {
+    //     if(res.authSetting['scope.userInfo']){
+    //       wx.getUserInfo({
+    //         success: function(res){
+    //           // success
+    //           // console.log(res.userInfo);
+    //         },
+    //         fail: function() {
+    //           // fail
+    //         },
+    //         complete: function() {
+    //           // complete
+    //         }
+    //       })
+    //     }
+    //   }
+    // });
       
   },
 
