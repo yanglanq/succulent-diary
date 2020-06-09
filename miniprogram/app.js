415421257a1f352ec503edfbe393eb76ff97a1e3
WH-ccn <1559677035@qq.com>
2020-06-09 17:11:51 +0800

    日记本封面修改

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -16,9 +16,31 @@ App({
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+
 
     var that = this;
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              that.globalData.wxuserInfo = res.userInfo;
+              if (that.userInfoReadyCallback) {
+                that.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+
+
+
     // 登录
     wx.login({
       success(res) {
@@ -52,6 +74,14 @@ App({
                         console.log(res);
                       },
                     })
+                  },
+                  fail: function () {
+                    // fail
+                    console.log("获取失败！")
+                  },
+                  complete: function () {
+                    // complete
+                    console.log("获取用户信息完成！")
                   }
                 })
               }
@@ -64,24 +94,7 @@ App({
       }
     })
 
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              that.globalData.wxuserInfo = res.userInfo;
-              if (that.userInfoReadyCallback) {
-                that.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
+    
   globalData: {
     userInfo: {},
     wxuserInfo: {},
